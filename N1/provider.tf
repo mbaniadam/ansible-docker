2a6dd87bee07a1804711be548cb61687658d9cc1
بنی آدم مرتضی <baniadam@Behsazan.net>
2023-08-30 13:42:21 +0330

    provider

--- N1/provider.tf
+++ N1/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.42.1"
+    }
+  }
+}
+
+
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.token
