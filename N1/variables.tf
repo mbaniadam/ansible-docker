a2c0cf6f653a6c3dde49e0ea00d837299920d6be
بنی آدم مرتضی <baniadam@Behsazan.net>
2023-08-30 13:51:27 +0330

    vars

--- N1/variables.tf
+++ N1/variables.tf
@@ -1,3 +1,29 @@
 variable "token" {
-  sensitive = true # Requires terraform >= 0.14
+  type      = string
+  sensitive = true
+}
+
+variable "location" {
+  type    = string
+  default = "nbg1"
+}
+
+variable "server_type" {
+  type    = string
+  default = "cx11"
+}
+
+variable "os_type" {
+  type    = string
+  default = "ubuntu-22.04"
+}
+
+variable "disk_size" {
+  type    = string
+  default = "20"
+}
+
+variable "ip_range" {
+  type    = string
+  default = "10.0.1.0/24"
 }
