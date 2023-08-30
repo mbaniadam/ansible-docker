aafe0b78f6074257c56c3204857f9a0e0ec805d3
mbaniadam <mortalbm@gmail.com>
2023-08-30 23:51:05 +0330

    list disk

--- N3/main.py
+++ N3/main.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
-import subprocess
 from datetime import datetime
-
+import linux_metrics 
+import subprocess
 
 app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def get_metrics():
     date_time = datetime.now()
     current_time = date_time.strftime("%d-%m-%Y   %H:%M:%S")
-    return '''The current date and time is: {}'''.format(current_time)
+    res_ls_disk = jsonify(subprocess.Popen('df -h', shell=True))
 
+    return '''The current date and time is: {}'''.format(current_time,res_ls_disk)
 
 app.run()
