57ba6430a978544f8126649364d3de7c48a6b7ea
بنی آدم مرتضی <baniadam@Behsazan.net>
2023-08-31 17:05:56 +0330

    ls disk

--- N3/main.py
+++ N3/main.py
@@ -8,17 +8,19 @@ app = Flask(__name__)
 def get_metrics():
     date_time = datetime.now()
     current_time = date_time.strftime("%d-%m-%Y   %H:%M:%S")
+    disk_info = []
     d_partitions = psutil.disk_partitions()
     for partition in d_partitions:
-        print(partition)
-        partition_info = {}
-
-    
-
+        partition_info = {
+            "name": partition.device,
+            "disk_size": psutil.disk_usage(partition.mountpoint).total,
+            "free_space": psutil.disk_usage(partition.mountpoint).free,
+        }
+        disk_info.append(partition_info)
 
     return '''The current date and time is: {}\n
     Disk info:\n{}\n
-    '''.format(current_time,d_partitions)
+    '''.format(current_time,disk_info)
 
 
 app.run(host='0.0.0.0',port=5000)
