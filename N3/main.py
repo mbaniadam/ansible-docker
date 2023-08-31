1cfcb54c4a5bcf933277d9c53bd0752c6c9ccdb1
بنی آدم مرتضی <baniadam@Behsazan.net>
2023-08-31 15:54:23 +0330

    ls disk

--- N3/main.py
+++ N3/main.py
@@ -4,12 +4,19 @@ import linux_metrics
 import subprocess
 import json
 
+
+def run_shell_command(command):
+    result = subprocess.run(command, capture_output=True)
+    return result.stdout
+
+
 app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def get_metrics():
     date_time = datetime.now()
     current_time = date_time.strftime("%d-%m-%Y   %H:%M:%S")
-    sub_result = subprocess.Popen('df -h', shell=True, stdout=subprocess.PIPE).communicate()[0]
+    #sub_result = subprocess.Popen('df -h', shell=True, stdout=subprocess.PIPE).communicate()[0]
+    sub_result = run_shell_command("df -h")
     js_result = json.loads(sub_result)
     return '''The current date and time is: {}
     df -h output:\n
