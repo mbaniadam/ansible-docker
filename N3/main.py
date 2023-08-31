2d576ad3f9412409acca233234638b9e81d484fe
بنی آدم مرتضی <baniadam@Behsazan.net>
2023-08-31 15:39:33 +0330

    ls disk

--- N3/main.py
+++ N3/main.py
@@ -10,7 +10,7 @@ def get_metrics():
     date_time = datetime.now()
     current_time = date_time.strftime("%d-%m-%Y   %H:%M:%S")
     sub_result = subprocess.Popen('df -h', shell=True, stdout=subprocess.PIPE).communicate()[0]
-    js_result = json.dump(sub_result)
+    js_result = json.loads(sub_result)
     return '''The current date and time is: {}
     df -h output:\n
     {}'''.format(current_time,js_result)
