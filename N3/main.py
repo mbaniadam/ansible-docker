543a772beaa74763b393999af99c26c50519c3c6
بنی آدم مرتضی <baniadam@Behsazan.net>
2023-08-31 16:10:44 +0330

    ls disk

--- N3/main.py
+++ N3/main.py
@@ -1,13 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from datetime import datetime
 import linux_metrics 
 import subprocess
 import json
-
-
-def run_shell_command(command):
-    result = subprocess.run(command, capture_output=True)
-    return result.stdout
 
 
 app = Flask(__name__)
@@ -15,11 +10,26 @@ app = Flask(__name__)
 def get_metrics():
     date_time = datetime.now()
     current_time = date_time.strftime("%d-%m-%Y   %H:%M:%S")
-    #sub_result = subprocess.Popen('df -h', shell=True, stdout=subprocess.PIPE).communicate()[0]
-    sub_result = run_shell_command("df -h")
+    sub_result = subprocess.Popen('df -h', shell=True, stdout=subprocess.PIPE).communicate()[0]
     js_result = json.loads(sub_result)
     return '''The current date and time is: {}
     df -h output:\n
     {}'''.format(current_time,js_result)
+
+
+@app.route('/list', methods=["GET"])
+def execute_command():
+    try:
+        command = "df -h"
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+        if result.returncode == 0:
+            output = result.stdout
+        else:
+            output = result.stderr
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+    return jsonify({"output": output})
+
 
 app.run(host='0.0.0.0',port=5000)
